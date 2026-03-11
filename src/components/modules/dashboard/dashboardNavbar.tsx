79d6a7ad0c08294e3644f298c492f31b1c365645
tasniaarhamarham <tasniaarhamarham@gmail.com>
2026-03-11 22:57:39 +0600

    dasboard sidebar and navbar demo design complete

--- src/components/modules/dashboard/dashboardNavbar.tsx
+++ src/components/modules/dashboard/dashboardNavbar.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import DashboardNavbarContent from './dashboardNavbarContent';
+import { getUserInfo } from '@/services/auth.service';
+// import { getNavItemsByRole } from '@/lib/navItem';
+import { defaultRoute } from '@/lib/auth.utils';
 
-const DashboardNavbar = () => {
-  return (
-    <div>
-      <h1>Dashboard Navbar</h1>
-    </div>
-  );
+const DashboardNavbar =async () => {
+    const userInfo=await getUserInfo();
+    console.log("userInfo",userInfo);
+    const dashboardHome=defaultRoute(userInfo?.role as string);
+    return (
+        <div>
+            <DashboardNavbarContent userInfo={userInfo} role={userInfo?.role as string} dashboardHome={dashboardHome} />
+        </div>
+    );
 };
 
 export default DashboardNavbar;
